055527f24d9830c55a826b87a4c38ce8694b7a5a
HankkaMark <markjish@163.com>
2022-11-17 22:56:10 -0500

    coverage 100

---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,21 @@
+test_that(
+  "The hello() returns Hello! for no input.",
+  {
+    p=hello()
+    expect_equal(p, "Hello!")
+  }
+)
+test_that(
+  "The hello() returns name,Hello! for input.",
+  {
+    p=hello('Mark')
+    expect_equal(p, "Hello Mark!")
+  }
+)
+test_that(
+  "The hello() returns nothing for invisible",
+  {
+    p=hello('Mark',TRUE)
+    expect_equal(p, "Hello Mark!")
+  }
+)
--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -14,12 +14,22 @@ test_that(
     expect_error(accel_plot(iris))
   }
 )
-
+test_that(
+  "The accel_plot() returns a ggplot object for freq.",
+  {
+    data(ukb_accel)
+    colnames(ukb_accel) <- c('freq','X','Y','Z')
+    # ukb_accel['freq'] = 0
+    # ukb_accel = subset(ukb_accel,select=-c(time))
+    p =  accel_plot(ukb_accel[1:100,])
+    expect_true(inherits(p, "gg"))
+  }
+)
 test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
     p = accel_plot(ukb_accel[1:100,])
-    expect_doppelganger("first-100-samples", p)
+    vdiffr::expect_doppelganger("first-100-samples", p)
   }
 )
